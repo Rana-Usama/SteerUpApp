177728c10e74bae99e1b51692f6cb4200699189c
RANA-USAMA <ranausamauet3430@gmail.com>
2021-09-22 06:01:47 -0700

    Home page Ready

--- app/screens/TypeIndustryScreen.js
+++ app/screens/TypeIndustryScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View, ImageBackground, Image } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 
@@ -8,34 +8,105 @@ import Screen from './../components/Screen';
 //config
 import Colors from '../config/Colors';
 import { RFPercentage } from 'react-native-responsive-fontsize';
+import InputField from './../components/common/InputField';
+import MyAppButton from './../components/common/MyAppButton';
+import LoadingModal from './../components/common/LoadingModel';
 
 function TypeIndustryScreen(props) {
+
+    const [indicator, showIndicator] = useState(false);
+
+    const [inputFields, setInputFeilds] = useState([
+        {
+            id: 0,
+            placeHolder: "Enter Industry Name",
+            value: '',
+            secure: false,
+            icon: false
+        },
+
+    ]);
+
+    const handleChange = (text, id) => {
+        const tempFeilds = [...inputFields];
+        tempFeilds[id].value = text;
+        setInputFeilds(tempFeilds);
+    }
+
+    const handleLogin = () => {
+        showIndicator(true);
+
+        try {
+            // API integration!
+
+        } catch (error) {
+            alert("Can't Proceed");
+        }
+
+        showIndicator(false);
+    };
+
+
+
     return (
         <Screen style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center', backgroundColor: Colors.white }}>
+            <LoadingModal show={indicator} />
             {/* Top Image */}
-            <ImageBackground style={{ alignItems: 'center', justifyContent: 'flex-start', width: '100%', height: RFPercentage(38) }} source={require('../../assets/Images/top.png')}>
+            <ImageBackground style={{ alignItems: 'center', justifyContent: 'center', width: '100%', height: RFPercentage(38) }} source={require('../../assets/Images/top.png')}>
                 <View style={{ backgroundColor: Colors.white, position: 'absolute', bottom: 0, width: '100%', height: RFPercentage(6), borderTopLeftRadius: RFPercentage(3), borderTopRightRadius: RFPercentage(3) }}>
                 </View>
-                <View style={{ marginTop: RFPercentage(6.5), alignItems: 'center', justifyContent: 'center' }}>
-                    <Text style={{ fontFamily: 'Philosopher_700Bold', color: Colors.white, fontSize: RFPercentage(6) }}>
+                <View style={{ marginBottom: RFPercentage(6), alignItems: 'center', justifyContent: 'center' }}>
+                    <Text style={{ fontFamily: 'Philosopher_700Bold', color: Colors.white, fontSize: RFPercentage(6.5) }}>
                         Steer Up
                     </Text>
-                    <Text style={{ fontWeight: 'bold', color: "#FFF", fontSize: RFPercentage(1.9), marginTop: RFPercentage(1.4) }}>
+                    <Text style={{ fontWeight: '200', color: "#FFF", fontSize: RFPercentage(2), marginTop: RFPercentage(0.5) }}>
                         B2B online platform to connect
                     </Text>
-                    <Text style={{ fontWeight: 'bold', color: "#FFF", fontSize: RFPercentage(1.9), marginTop: RFPercentage(0.2) }}>
+                    <Text style={{ fontWeight: '200', color: "#FFF", fontSize: RFPercentage(2), marginTop: RFPercentage(0) }}>
                         businesses Across borders
                     </Text>
                 </View>
             </ImageBackground>
 
             {/* Main Heading */}
-            <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
-                <FontAwesome name="industry" style={{ fontSize: RFPercentage(2.8), color: '#025e6c' }} color="black" />
-                <Text style={{ marginLeft: RFPercentage(2), fontSize: RFPercentage(3.7) }}>Type Your Industry</Text>
+            <View style={{ marginTop: RFPercentage(10), flexDirection: 'row', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+                <FontAwesome name="industry" style={{ marginRight: RFPercentage(2), fontSize: RFPercentage(2.8), color: '#025e6c' }} color="black" />
+                <Text style={{ fontSize: RFPercentage(3.9) }}>
+                    Type Your Industry
+                </Text>
             </View>
-            {/* Input Field */}
 
+            {/* Input Fields */}
+            <View style={{ width: "80%", justifyContent: "center", alignItems: "center", marginTop: RFPercentage(8) }}>
+                {inputFields.map((item, i) =>
+                    <View key={i} style={{ marginTop: i == 0 ? RFPercentage(1) : RFPercentage(3), width: "100%" }} >
+                        <InputField
+                            backgroundColor={Colors.light}
+                            placeHolder={item.placeHolder}
+                            width="100%"
+                            height={RFPercentage(6.3)}
+                            value={item.value}
+                            onChange={(text) => handleChange(text, item.id)}
+                            secure={item.secure}
+                            icon={item.icon}
+                        />
+                    </View>
+                )}
+            </View>
+
+            {/* Button */}
+            <View style={{ width: "100%", justifyContent: 'center', alignItems: "center", marginTop: RFPercentage(4) }}>
+                <MyAppButton
+                    title="SEARCH"
+                    bold={true}
+                    borderRadius={RFPercentage(2)}
+                    padding={RFPercentage(1.5)}
+                    onPress={() => handleLogin()}
+                    backgroundColor={"#12424a"}
+                    color={Colors.white}
+                    width={"55%"}
+                />
+            </View>
         </Screen>
     );
 }
