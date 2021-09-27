6521b9bebc50b92f5848ee61619c11f3bc50c055
RANA-USAMA <ranausamauet3430@gmail.com>
2021-09-28 02:21:27 +0500

    picker added

--- app/screens/TypeIndustryScreen.js
+++ app/screens/TypeIndustryScreen.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import { Text, View, KeyboardAvoidingView, ScrollView, StatusBar } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import { RFPercentage } from 'react-native-responsive-fontsize';
+import ReactNativeCrossPicker from "react-native-cross-picker";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+
 
 //components
 import Screen from './../components/Screen';
-import InputField from './../components/common/InputField';
 import MyAppButton from './../components/common/MyAppButton';
-import LoadingModal from './../components/common/LoadingModel';
 import MyAppbar from '../components/common/MyAppbar';
 import HelpButton from '../components/common/HelpButton';
 
@@ -16,117 +18,95 @@ import Colors from '../config/Colors';
 
 function TypeIndustryScreen(props) {
 
+
+    const [selectedItem, setItem] = useState('')
+
+    const items = [
+        { label: "Information Technology", value: "Information Technology" },
+        { label: "Healthcare", value: "Healthcare" },
+        { label: "Manufacturing", value: "Manufacturing" },
+        { label: "Financials", value: "Financials" },
+        { label: "Consumer Discretionary", value: "Consumer Discretionary" },
+        { label: "Turnkey Industrials", value: "Turnkey Operations" },
+        { label: "Communication Services", value: "Communication Services" },
+        { label: "Consumer Stables", value: "Consumer Stables" },
+        { label: "Materials", value: "Materials" },
+        { label: "Energy", value: "Energy" },
+        { label: "Utilities", value: "Utilities" },
+        { label: "Real Estate", value: "Real Estate" },
+
+    ]
+
+    const iconComponent = () => {
+        return <MaterialCommunityIcons
+            name={"chevron-down"}
+            size={20}
+            color={"grey"}
+        />
+    }
+
+
     const [indicator, showIndicator] = useState(false);
     const [helpButton, setHelpButton] = useState(true);
 
-    const [inputFields, setInputFeilds] = useState([
-        {
-            placeHolder: "Enter Industry Name",
-            value: '',
-        },
 
-    ]);
-
-    const handleChange = (text, id) => {
-        const tempFeilds = [...inputFields];
-        tempFeilds[id].value = text;
-        setInputFeilds(tempFeilds);
-    }
-
-    const handleLogin = () => {
-        showIndicator(true);
-
-        let tempfeilds = [...inputFields];
-
-        if (tempfeilds[0].value === "") {
-            alert("Please write atleast one industry name");
-            showIndicator(false);
-            return true;
-        }
-        props.navigation.navigate("EmergingMarketsScreen")
-
-        try {
-            // API integration!
-
-        } catch (error) {
-            alert("Can't Proceed");
-        }
-
-        showIndicator(false);
-    };
 
 
 
     return (
-        <>
-            <KeyboardAvoidingView
-                behavior={Platform.OS === "ios" ? "padding" : "height"}
-                style={{ flex: 1 }}
-            >
-                <Screen style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center', backgroundColor: Colors.white }}>
-                    <StatusBar barStyle="dark-content" />
-                    <LoadingModal show={indicator} />
-                    {/* App bar */}
-                    <MyAppbar />
-                    {/* Scroll View starts */}
-                    <ScrollView style={{ backgroundColor: Colors.white, flex: 1, width: '100%' }} >
-                        <View style={{ justifyContent: 'center', alignItems: 'center' }}  >
-                            {/* Main Heading */}
-                            <View style={{ marginTop: RFPercentage(6), flexDirection: 'row', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
-                                <FontAwesome name="industry" style={{ right: RFPercentage(2), fontSize: RFPercentage(2.8), color: '#025e6c' }} color="black" />
-                                <Text style={{ fontSize: RFPercentage(3.9) }}>
-                                    Type Your Industry
-                                </Text>
-                            </View>
-                            <KeyboardAvoidingView
-                                behavior={Platform.OS === "ios" ? "padding" : "height"}
-                                style={{ flex: 1 }}
-                            >
-                                {/* Input Fields */}
-                                <View style={{ width: "80%", justifyContent: "center", alignItems: "center", marginTop: RFPercentage(8) }}>
-                                    {inputFields.map((item, i) =>
-                                        <View key={i} style={{ marginTop: i == 0 ? RFPercentage(1) : RFPercentage(3), width: "100%" }} >
-                                            <InputField
-                                                placeholder={item.placeHolder}
-                                                backgroundColor={Colors.inputFieldBackgroundColor}
-                                                placeholderColor={"#82867D"}
-                                                onTouchStart={() => setHelpButton(false)}
-                                                onTouchEnd={() => setHelpButton(true)}
-                                                borderColor={Colors.white}
-                                                borderRadius={RFPercentage(1)}
-                                                fontSize={RFPercentage(2)}
-                                                handleFeild={(text) => handleChange(text, i)}
-                                                value={item.value}
-                                                width={"100%"}
-                                            />
-                                        </View>
-                                    )}
-                                </View>
-                            </KeyboardAvoidingView>
-                            {/* Button */}
-                            <View style={{ width: "100%", justifyContent: 'center', alignItems: "center", marginTop: RFPercentage(4) }}>
-                                <MyAppButton
-                                    title="SEARCH"
-                                    bold={true}
-                                    borderRadius={RFPercentage(2)}
-                                    padding={RFPercentage(1.5)}
-                                    onPress={() => handleLogin()}
-                                    backgroundColor={Colors.secondary}
-                                    color={Colors.white}
-                                    width={"55%"}
-                                />
-                            </View>
 
-                        </View>
-                    </ScrollView>
-                    {helpButton ?
-                        <HelpButton />
-                        :
-                        null
-                    }
-                </Screen>
-            </KeyboardAvoidingView>
-        </>
+        <Screen style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center', backgroundColor: Colors.white }}>
+            <StatusBar barStyle="dark-content" />
+            {/* App bar */}
+            <MyAppbar />
+            {/* Scroll View starts */}
+            <ScrollView style={{ backgroundColor: Colors.white, flex: 1, width: '100%' }} >
+                <View style={{ justifyContent: 'center', alignItems: 'center' }}  >
+                    {/* Main Heading */}
+                    <View style={{ marginTop: RFPercentage(6), flexDirection: 'row', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+                        <FontAwesome name="industry" style={{ right: RFPercentage(2), fontSize: RFPercentage(2.8), color: '#025e6c' }} color="black" />
+                        <Text style={{ fontSize: RFPercentage(3.9) }}>
+                            Choose Your Industry
+                        </Text>
+                    </View>
+
+                    <View style={{ marginTop: RFPercentage(8), width: '100%', alignItems: 'center', justifyContent: 'center' }}>
+                        <ReactNativeCrossPicker
+                            modalTextStyle={{ color: "rgb(0, 74, 173)" }}
+                            mainComponentStyle={{ width: "85%", borderWidth: 0, backgroundColor: Colors.inputFieldBackgroundColor }}
+                            modalComponentStyle={{ borderRadius: RFPercentage(3), backgroundColor: Colors.white, borderColor: Colors.grey, borderWidth: RFPercentage(0.1) }}
+                            iconComponent={iconComponent}
+                            placeholderStyle={{ color: "black" }}
+                            modalTextStyle={{ color: "#12424a", fontSize: RFPercentage(2.6), fontWeight: 'bold' }}
+                            items={items}
+                            setItem={setItem} selectedItem={selectedItem}
+                            placeholder="Tap to choose"
+                            modalMarginTop={"100%"}
+                        />
+                    </View>
+                    {/* Button */}
+                    <View style={{ width: "100%", justifyContent: 'center', alignItems: "center", marginTop: RFPercentage(4) }}>
+                        <MyAppButton
+                            title="NEXT"
+                            bold={true}
+                            borderRadius={RFPercentage(2)}
+                            padding={RFPercentage(1.5)}
+                            onPress={() => props.navigation.navigate("EmergingMarketsScreen")}
+                            backgroundColor={Colors.secondary}
+                            color={Colors.white}
+                            width={"55%"}
+                        />
+                    </View>
+
+                </View>
+            </ScrollView>
+            {helpButton ?
+                <HelpButton />
+                :
+                null
+            }
+        </Screen>
+
     );
 }
 
