15e1b4c8a50ccdd33fd615cfe2c75194ebfb034d
zahidalidev <m.zahidalidev@gmail.com>
2021-10-22 17:20:40 +0500

    completed

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,45 +1,20 @@
-'use strict';
-const excelToJson = require('convert-excel-to-json');
+const express = require('express');
+const cors = require('cors')
+const IndustryRankRoute = require('./routes/IndustryRankRoute');
 
-// const fs = require('fs');
-// const csv = require('csvtojson');
+const app = express();
 
+// using cors to for public API
+app.use(cors())
 
-// let cleanedData = []
-
-// const readCSV = async () => {
-//     let jsonArray = await csv().fromFile('./assets/itdataset.csv');
-//     jsonArray = jsonArray.sort(function (a, b) {
-//         return parseInt(a.Time) - parseInt(b.Time);
-//     })
-//     console.log(jsonArray[990])
-// }
+app.use(express.json());
 
 
-const result = excelToJson({
-    sourceFile: 'assets/itdata.xlsx'
+app.set('port', (process.env.PORT || 5000));
+
+// routes
+app.use('/api/industryrank', IndustryRankRoute);
+
+app.listen(app.get("port"), function () {
+    console.log("Node app is running on port " + app.get('port'));
 });
-
-
-let cleanedData = []
-
-for (let i = 1; i < result['OECD.Stat export'].length; i++) {
-    const tempObj = result['OECD.Stat export'][i];
-    const lastYearKey = Object.keys(tempObj)[Object.keys(tempObj).length - 1];
-    const latestCountryRank = result['OECD.Stat export'][i][lastYearKey];
-
-    const filteredObject = {};
-    if (latestCountryRank !== '..') {
-        filteredObject.country = result['OECD.Stat export'][i].A;
-        filteredObject.latestYearRank = latestCountryRank;
-
-        cleanedData.push(filteredObject);
-    }
-
-
-}
-
-let sortedData = cleanedData.sort(function (a, b) { return b.latestYearRank - a.latestYearRank });
-
-console.log(sortedData)
-// console.log(result['OECD.Stat export'][0]);
